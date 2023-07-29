1aaed8801d4a7b2f86cf196801a96a85cce63b81
SaiSeng18 <65032098+SaiSeng18@users.noreply.github.com>
2023-07-29 10:42:42 +0630

    second commit

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
 	images: {
 		domains: ["lh3.googleusercontent.com", "res.cloudinary.com", "task.com"],
 	},
-	// experimental: {
-	//   serverComponentsExternalPackages: ['cloudinary', 'graphql-request']
-	// },
+	experimental: {
+		serverComponentsExternalPackages: ["cloudinary", "graphql-request"],
+	},
 	eslint: {
 		// Warning: This allows production builds to successfully complete even if
 		// your project has ESLint errors.
