1ee08b87349632ce511fa3b7f53bd1c3ad2217cf
SaiSeng18 <65032098+SaiSeng18@users.noreply.github.com>
2023-07-29 10:09:05 +0630

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,22 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-	title: "Flexibble",
-	description: "Showcase and discover remarkable developer projects",
+export const metadata = {
+  title: "Flexibble",
+  description: "Showcase and discover remarkable developer projects",
 };
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<Navbar />
-				<main>{children}</main>
-				<Footer />
-			</body>
-		</html>
-	);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body>
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,42 +1,44 @@
-"use client";
+"use client"
 
-import { getProviders, signIn } from "next-auth/react";
-import React, { useEffect, useState } from "react";
+import { getProviders, signIn } from 'next-auth/react';
+import React, { useEffect, useState } from 'react'
+
+import Button from './Button';
 
 type Provider = {
-	id: string;
-	name: string;
-	type: string;
-	signinUrl: string;
-	callbackUrl: string;
-	singinUrlParams?: Record<string, string> | null;
-};
+    id: string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams?: Record<string, string> | undefined;
+  };
+  
+  type Providers = Record<string, Provider>;
 
-type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-	const [providers, setProviders] = useState<Providers | null>(null);
+    const [providers, setProviders] = useState<Providers | null>(null);
 
-	useEffect(() => {
-		const fetchProviders = async () => {
-			const res = await getProviders();
-			console.log(res);
-			setProviders(res);
-		};
-		fetchProviders();
-	}, []);
+    useEffect(() => {
+        const fetchProviders = async () => {
+            const res = await getProviders();
+    
+            setProviders(res);
+        }
 
-	if (providers) {
-		return (
-			<div>
-				{Object.values(providers).map((provider: Provider, i) => (
-					<button key={i} onClick={() => signIn(provider?.id)}>
-						{provider.id}
-					</button>
-				))}
-			</div>
-		);
-	}
-};
+        fetchProviders();
+    }, []);
 
-export default AuthProviders;
+    if (providers) {
+        return (
+            <div>
+                {Object.values(providers).map((provider: Provider, i) => (
+                    <Button key={i} title='Sign In' handleClick={() => signIn(provider?.id)} />
+                ))}
+            </div>
+        )
+    }
+}
+
+export default AuthProviders
